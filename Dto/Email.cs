c2dfe1b981809052451c1915bf970542d937536d
Navjot Singh <navjotsandhu910@outlook.com>
2023-04-03 15:26:47 +0530

    improve email service

---
+++ Dto/Email.cs
@@ -0,0 +1,14 @@
+
+namespace WebApplication3.Dto
+{
+    public class Email
+    {
+
+        public string From { get; set; }
+        public string To {get;set;}
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+
+}
